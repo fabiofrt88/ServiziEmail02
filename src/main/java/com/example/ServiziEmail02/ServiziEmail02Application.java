e98497f725939a639a79e6f7dc3f45620d0fe1b3
Fabio Frattarelli <fabiofrt@outlook.it>
2023-02-18 08:54:44 +0100

    first comm

---
+++ src/main/java/com/example/ServiziEmail02/ServiziEmail02Application.java
@@ -0,0 +1,13 @@
+package com.example.ServiziEmail02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiziEmail02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiziEmail02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ServiziEmail02/ServiziEmail02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServiziEmail02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiziEmail02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
